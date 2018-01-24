8311615ecc4cae6fd95c859493af7aad60d2ef42
browniApp <wanpracha.n@rmutsv.ac.th>
2018-01-24 11:31:50 +0700

    pincode and delay time

--- BowBlink.ino
+++ BowBlink.ino
@@ -1,13 +1,23 @@
+//ส่วนของการประกาศตัวแปร
+int myOutput = D0;
+int myDelayTime = 2000; //ค่าของเวลาจะมีหน่วยเป็น มิลิวินาที
 
+//ส่วนของ setup คือ ส่วนที่กำหนดค่าคงที่ ที่จะใช้ในโค้ด จะทำงานครั้งแรก ครั้งเดียว
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(16, OUTPUT);
-}
 
+//  ส่วนที่กำหนด การส่ง Digital Out จากขาของ Node
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(myOutput, OUTPUT);
+
+
+  
+}// end of setup
+
+//ส่วนของ loop จะทำงานวนไปมาตาม digital logic
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(16, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(16, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(myOutput, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(myDelayTime);                       // wait for a second
+  digitalWrite(myOutput, LOW);    // turn the LED off by making the voltage LOW
+  delay(myDelayTime);                       // wait for a second
 }
